4481e4978914677b81178611f8525eabac576dfc
AM Grobelny <allenmichaelgrobelny@gmail.com>
2020-11-03 20:49:19 -0600

    Testing metadata object

--- model_update_user_request.go
+++ model_update_user_request.go
@@ -8,10 +8,11 @@
  */
 
 package gohiarc
+
 // UpdateUserRequest struct for UpdateUserRequest
 type UpdateUserRequest struct {
-	Key string `json:"key,omitempty"`
-	Name string `json:"name,omitempty"`
-	Description string `json:"description,omitempty"`
-	Metadata map[string]map[string]interface{} `json:"metadata,omitempty"`
+	Key         string                 `json:"key,omitempty"`
+	Name        string                 `json:"name,omitempty"`
+	Description string                 `json:"description,omitempty"`
+	Metadata    map[string]interface{} `json:"metadata,omitempty"`
 }
